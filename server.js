7a2e77e4e2df8943833370acdbc1a9cf99d39ae5
Shamleena M P <shamlithariq@gmail.com>
2024-05-22 14:57:09 +0530

    route name updated

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(bodyParser.json());
 const module2Routes = () => {
   return (
-    app.get("/api/module2/route1", function (req, res) {
+    app.get("/module2/route1", function (req, res) {
       res.status(200).send({ data: "Module2 - route1 response" });
     }),
-    app.get("/api/module2/route2", function (req, res) {
+    app.get("/module2/route2", function (req, res) {
       res.status(200).send({ data: "Module2 - route2 response" });
     })
   );
